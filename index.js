8ed06490e7dffaa2e01ed322c3f26aa619cff1b4
Jason Dumont <jason.dumont@pc-206-66.customer.warehouse.aqh.lan>
2017-01-21 14:15:13 +0000

    another alteration

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 app.get('/', (req, res) => {
-  res.send('HEY! There Chciken')
+  res.send('HEY! There Chciken');
+  res.send('you suck and this aws thing is hards');
 })
 app.listen(3000, () => console.log('Server running on port 3000'))
