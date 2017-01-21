4eb0ef9fdee2e224f6095f6045cc01b65c67af29
Jason Dumont <jason.dumont@pc-206-66.customer.warehouse.aqh.lan>
2017-01-21 14:20:43 +0000

    updated a test site

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express')
 const app = express()
+const test = express();
+
+
 app.get('/', (req, res) => {
   res.send('HEY! There Chicken')
 
 })
 app.listen(3000, () => console.log('Server running on port 3000'))
+
+
+app.get('/', (req, res) => {
+  res.send('HEY! There Chicken, this is the test site')
+
+})
+app.listen(4321, () => console.log('Server running on port 4321'))
